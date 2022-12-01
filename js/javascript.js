c4ad51a20abba8168008d5dad98e6094d023bc77
Keenan <42125735+Styleshift@users.noreply.github.com>
2022-12-01 13:38:54 -0500

    assignment finished

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,20 @@
 console.log(23 + 97)
-let myNumber = 20 + 23 + 58 + 93 + 47 + 66
-alert (myNumber)
+
+//let myNumber = 20 + 23 + 58 + 93 + 47 + 66
+//alert (myNumber)
+
+console.log(20 + 23 + 58 + 93 + 47 + 66)
+
+console.log((4 + 6 + 9) / 77)
+
+let a = 10
+console.log(a)
+
+console.log (9 * a)
+
+let b = 7 * a
+console.log(b)
+
+const max = 57
+let actual = max - 13
+let percentage = actual / max
